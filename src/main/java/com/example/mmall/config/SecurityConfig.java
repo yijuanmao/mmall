24f88460776e622dd78858932848b5c5c8385f1b
zhener <824414828@qq.com>
2019-10-25 16:49:10 +0800

    增加rabbitmq

--- src/main/java/com/example/mmall/config/SecurityConfig.java
+++ src/main/java/com/example/mmall/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/v2/api-docs/**"
                 )
                 .permitAll()
-                .antMatchers("/login", "/login/register","/response")// 对登录注册要允许匿名访问
+                .antMatchers("/login", "/login/register","/response","/user/getMenu")// 对登录注册要允许匿名访问
                 .permitAll()
                 .antMatchers(HttpMethod.OPTIONS)//跨域请求会先进行一次options请求
                 .permitAll()
